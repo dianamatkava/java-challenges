30837e6c52104fe1e74a9c0a9869c199722245f2
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-09-02 08:48:47 +0200

    Rotate

--- RightRotation/RightRotation.java
+++ RightRotation/RightRotation.java
@@ -4,17 +4,29 @@ import java.io.Console;
 import java.util.Arrays;
 
 
+// Sample Input 2:
+// 1 2 3 4 5
+// 2
+
+// 5 1 2 3 4
+
+// Sample Output 2:
+// 4 5 1 2 3
+
 class RightRotation {
     private static void rotate(int[] arr, int steps) {
         int shift = steps % arr.length;
         for (int shiftIndex = 0; shiftIndex < shift; shiftIndex++) {
             int temp = arr[arr.length-1];
-            System.out.println(temp);
-            for (int shiftIntem = arr.length - 1; shiftIntem > shiftIndex; shiftIntem--) {
-                arr[shiftIntem] = arr[shiftIntem-1];
+            arr[arr.length-1] = arr[0];
+
+            for (int shiftIntem = shiftIndex; shiftIntem < arr.length; shiftIntem++) {
+                int tempShift = arr[shiftIntem];
+                arr[shiftIntem] = arr[arr.length-1];
+                arr[arr.length-1] = tempShift;
             }
 
-            arr[shiftIndex] = temp;
+            arr[0] = temp;
             System.out.println(Arrays.toString(arr));
         }
     }
