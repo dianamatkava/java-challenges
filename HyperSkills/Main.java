5c0a78cbf0f3aee795dc10a7a24918547157ae43
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-09-19 10:19:42 +0200

    update

--- HyperSkills/Main.java
+++ HyperSkills/Main.java
@@ -1,32 +1,34 @@
 package HyperSkills;
-import java.util.Random;
-
-
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
+        // Read an integer from the input
+        String integerStr = reader.readLine();
+        int integer;
+        try {
+            integer = Integer.parseInt(integerStr);
+        } catch (Exception e) {
+            System.out.println("Invalid input!");
+            return;
+        }
+        
 
-        MeClass c = new MeClass();
+        // Read a string from the input
+        String string = reader.readLine();
+        if (integer > 0) {
+            for (int i = 0; i <= integer; i++ ) {
+                System.out.println(string);
+            }
+        } else if (integer == 0) {
+            System.out.println(string);
+        }
+        // } 
+        // else {
+        //     System.out.println("Invalid input!");
+        // }
 
-        Random random = new Random(100000);
-        System.out.println(random.nextInt(5)); 
-    }
-}
-
-
-class MeClass implements Flying {
-    public int getHeight() {
-        return 100; 
-    }
-}
-
-interface Flying {
-    // returns height of flying in meters
-    int getHeight();
-
-    // implements a default method getHeightInKm that returns height of flying in km as int type
-    default int getHeightInKm () {
-        return getHeight() / 1000;
     }
 }
