0d7bced6f78019b5ad4186048456f3707025abda
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2024-10-30 08:34:40 +0100

    Complete Sales Calculator

--- SalesCalculator/SalesCalculator.java
+++ SalesCalculator/SalesCalculator.java
@@ -1,34 +1,45 @@
 package HyperSkillsFirstProject;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 
 class Product {
-    String name;
-    float price;
+    private String name;
+    private float price;
 
     public Product (String name, float price) {
         this.name = name;
         this.price = price;
     }
 
-}
-
-class Order{
-    Product product;
-    int amount;
-    int price;
-
-    public float getPrice() {
-        return this.product.price * this.amount;
+    public float getPrice () {
+        return this.price;
     }
 
-    public Order(Product product, int amount) {
+    public String getName () {
+        return this.name;
+    }
+}
+
+class LineItem{
+    private Product product;
+    private int amount;
+
+    public float getPrice() {
+        return this.product.getPrice() * this.amount;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public LineItem(Product product, int amount) {
         this.product = product;
         this.amount = amount;
     }
 
-    public Order(Product product) {
+    public LineItem(Product product) {
         this.product = product;
         this.amount = 1;
     }
@@ -36,24 +47,24 @@ class Order{
 
 
 class Sales {
-    ArrayList<Order> orders;
-    float expences = 0;
+    public ArrayList<LineItem> orders;
+    private float expenses = 0;
 
-    public Sales (ArrayList<Order> orders) {
+    public Sales (ArrayList<LineItem> orders) {
         this.orders = orders;
     }
 
-    public void placeOrder (Order order) {
+    public void placeOrder (LineItem order) {
         this.orders.add(order);
     }
 
-    public void setExpence (float expence) {
-        this.expences = expence;
+    public void setExpense (float expense) {
+        this.expenses += expense;
     }
 
     public float getIncome() {
         float totalIncome = 0;
-        for (Order order : this.orders) {
+        for (LineItem order : this.orders) {
             totalIncome += order.getPrice();
         }
         return totalIncome;
@@ -61,15 +72,15 @@ class Sales {
 
     public float getTotalIncome () {
         float income = getIncome();
-        return income - this.expences;
+        return income - this.expenses;
     }
 
     public void printAllSales() {
         if (this.orders.size() > 0) {
             System.out.println("Earned amount:");
         }
-        for (Order order : this.orders) {
-            System.out.println(order.product.name + ": $" + (int) order.getPrice());
+        for (LineItem order : this.orders) {
+            System.out.println(order.getProduct().getName() + ": $" + (int) order.getPrice());
         }
         System.out.println("Income: $" + (int) this.getTotalIncome());
     }
@@ -80,17 +91,28 @@ class Sales {
 public class SalesCalculator {
     public static void main(String[] args) {
 
-        Order bubblegum = new Order(new Product("Bubblegum", 202));
-        Order toffee = new Order(new Product("Toffee", 118));
-        Order iceCream = new Order(new Product("Ice cream", 2250));
-        Order milkChocolate = new Order(new Product("Milk chocolate", 1680));
-        Order doughnut = new Order(new Product("Doughnut", 1075));
-        Order pancake = new Order(new Product("Pancake", 80));
+        Scanner scanner = new Scanner(System.in);
 
-        Sales sales = new Sales(new ArrayList<Order>(
+        LineItem bubblegum = new LineItem(new Product("Bubblegum", 202));
+        LineItem toffee = new LineItem(new Product("Toffee", 118));
+        LineItem iceCream = new LineItem(new Product("Ice cream", 2250));
+        LineItem milkChocolate = new LineItem(new Product("Milk chocolate", 1680));
+        LineItem doughnut = new LineItem(new Product("Doughnut", 1075));
+        LineItem pancake = new LineItem(new Product("Pancake", 80));
+
+        Sales sales = new Sales(new ArrayList<LineItem>(
                 Arrays.asList(bubblegum, toffee, iceCream, milkChocolate, doughnut, pancake)
             )
         );
         sales.printAllSales();
+
+        System.out.println("Staff expenses:");
+        sales.setExpense(scanner.nextFloat());
+
+        System.out.println("Other expenses:");
+        sales.setExpense(scanner.nextFloat());
+
+        float netIncome = sales.getTotalIncome();
+        System.out.println("Net income: $" + (int) netIncome);
     }
 }
